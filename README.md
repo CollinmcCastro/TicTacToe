908a594038d433f726d3f92f3a7f610260012c03
Collin Cabral-Castro <150752659+CollinmcCastro@users.noreply.github.com>
2023-11-17 01:46:37 -0500

    Play again & Close button
    Implemented two buttons to display after a game won/tie message that will prompt user to start another game or close the application.
    
    Also updated the ReadMe file.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# TicTacToe
- TicTacToe Project made in Java
+# Tic-Tac-Toe Game
+
+This is a simple implementation of the classic Tic-Tac-Toe game using Java Swing. The game features a graphical user interface with a 3x3 grid of buttons where two players can take turns making moves. The game declares a winner when a player has three symbols in a row (horizontally, vertically, or diagonally) or declares a tie if the board is full with no winner.
+
+## How to Play
+
+1. Run the program to launch the Tic-Tac-Toe game window.
+2. Click on the empty squares to make your move.
+3. The game alternates between 'X' and 'O' players.
+4. The game ends when one player wins or the board is full.
+
+## Features
+
+- Responsive GUI with a 3x3 grid of buttons.
+- Player turn alternates between 'X' and 'O'.
+- Displays a message dialog declaring the winner or a tie.
+- Play Again button to reset the game.
+- Close button to exit the game.
+
+## Getting Started
+
+Ensure you have Java installed on your machine.
+
+1. Clone or download the repository.
+2. Compile and run the `TicTacToeGame.java` file.
+3. Follow the on-screen instructions to play the game.
+
+## Files
+
+- `TicTacToeGame.java`: Main Java file containing the game logic and GUI.
+- `README.md`: Documentation describing the game and how to use it.
+
+Feel free to customize and improve the game as needed!
--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,15 +4,14 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-public class TicTacToeGame extends JFrame
-{
+public class TicTacToeGame extends JFrame {
     private JButton[][] buttons;
     private char currentPlayer;
     private boolean gameWon;
+    private JButton resetButton;
+    private JButton closeButton; // New button for closing the game
 
-    public TicTacToeGame()
-    {
+    public TicTacToeGame() {
         buttons = new JButton[3][3];
         currentPlayer = 'X';
         gameWon = false;
@@ -20,51 +19,75 @@ public class TicTacToeGame extends JFrame
         setTitle("Tic-Tac-Toe");
         setSize(300, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(3, 3));
+        setLayout(new BorderLayout());
 
         initializeButtons();
+        addResetButton(); // New method to add a reset button
+        addCloseButton(); // New method to add a close button
+
+        // Initially, make the reset and close buttons invisible
+        resetButton.setVisible(false);
+        closeButton.setVisible(false);
     }
 
-    private void initializeButtons()
-    {
-        for (int row = 0; row < 3; row++)
-        {
-            for (int col = 0; col < 3; col++)
-            {
+    private void initializeButtons() {
+        JPanel boardPanel = new JPanel(new GridLayout(3, 3));
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
                 buttons[row][col] = createButton();
-                add(buttons[row][col]);
+                boardPanel.add(buttons[row][col]);
             }
         }
+
+        add(boardPanel, BorderLayout.CENTER);
     }
 
-    private JButton createButton()
-    {
+    private void addResetButton() {
+        resetButton = new JButton("Play Again");
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resetGame();
+            }
+        });
+
+        // Use a separate panel with FlowLayout for the buttons
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(resetButton);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    private void addCloseButton() {
+        closeButton = new JButton("Close");
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                closeGame();
+            }
+        });
+
+        // Use the same panel for both buttons
+        JPanel buttonPanel = (JPanel) getContentPane().getComponent(1);
+        buttonPanel.add(closeButton);
+    }
+
+    private JButton createButton() {
         JButton button = new JButton();
         button.setFont(new Font("Arial", Font.PLAIN, 60));
-        button.addActionListener(new ActionListener()
-        {
+        button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                if (!gameWon)
-                {
+            public void actionPerformed(ActionEvent e) {
+                if (!gameWon) {
                     JButton clickedButton = (JButton) e.getSource();
-                    if (clickedButton.getText().equals(""))
-                    {
+                    if (clickedButton.getText().equals("")) {
                         clickedButton.setText(String.valueOf(currentPlayer));
                         clickedButton.setEnabled(false);
-                        if (checkWin())
-                        {
-                            JOptionPane.showMessageDialog(null, "Player " + currentPlayer + "wins!");
-                            gameWon = true;
-                        }
-                        else if (isBoardFull())
-                        {
-                            JOptionPane.showMessageDialog(null, "It's a tie!");
-                            gameWon = true;
-                        }
-                        else
-                        {
+                        if (checkWin()) {
+                            showGameOverDialog("Player " + currentPlayer + " wins!");
+                        } else if (isBoardFull()) {
+                            showGameOverDialog("It's a tie!");
+                        } else {
                             currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
                         }
                     }
@@ -74,44 +97,77 @@ public class TicTacToeGame extends JFrame
         return button;
     }
 
-    private boolean checkWin()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            if (buttons[i][0].getText().equals(String.valueOf(currentPlayer)) &&
-            buttons[i][1].getText().equals(String.valueOf(currentPlayer)) &&
-            buttons[i][2].getText().equals(String.valueOf(currentPlayer)) ||
-            buttons[0][i].getText().equals(String.valueOf(currentPlayer)) &&
-            buttons[1][i].getText().equals(String.valueOf(currentPlayer)) &&
-            buttons[2][i].getText().equals(String.valueOf(currentPlayer)) ||
-            buttons[0][0].getText().equals(String.valueOf(currentPlayer)) &&
-            buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
-            buttons[2][2].getText().equals(String.valueOf(currentPlayer)) ||
-            buttons[0][2].getText().equals(String.valueOf(currentPlayer)) &&
-            buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
-            buttons[2][0].getText().equals(String.valueOf(currentPlayer))) {
+    private void resetGame() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col].setText("");
+                buttons[row][col].setEnabled(true);
+            }
+        }
+        currentPlayer = 'X';
+        gameWon = false;
+
+        // Make the reset and close buttons invisible again after resetting the game
+        resetButton.setVisible(false);
+        closeButton.setVisible(false);
+    }
+
+    private void showGameOverDialog(String message) {
+        JOptionPane.showMessageDialog(null, message);
+        gameWon = true;
+
+        // Make the reset and close buttons visible when the game is over
+        resetButton.setVisible(true);
+        closeButton.setVisible(true);
+    }
+
+    private void closeGame() {
+        // Close the game
+        dispose();
+    }
+
+    private boolean checkWin() {
+        for (int i = 0; i < 3; i++) {
+            if (!buttons[i][0].getText().equals("") &&
+                    buttons[i][0].getText().equals(String.valueOf(currentPlayer)) &&
+                    buttons[i][1].getText().equals(String.valueOf(currentPlayer)) &&
+                    buttons[i][2].getText().equals(String.valueOf(currentPlayer)) ||
+                    !buttons[0][i].getText().equals("") &&
+                            buttons[0][i].getText().equals(String.valueOf(currentPlayer)) &&
+                            buttons[1][i].getText().equals(String.valueOf(currentPlayer)) &&
+                            buttons[2][i].getText().equals(String.valueOf(currentPlayer)) ||
+                    !buttons[0][0].getText().equals("") &&
+                            buttons[0][0].getText().equals(String.valueOf(currentPlayer)) &&
+                            buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+                            buttons[2][2].getText().equals(String.valueOf(currentPlayer)) ||
+                    !buttons[0][2].getText().equals("") &&
+                            buttons[0][2].getText().equals(String.valueOf(currentPlayer)) &&
+                            buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+                            buttons[2][0].getText().equals(String.valueOf(currentPlayer))) {
                 return true;
             }
-
-
-
-
-
         }
         return false;
     }
-    private boolean isBoardFull()
-    {
-        for (int row = 0; row < 3; row++)
-        {
-            for (int col = 0; col < 3; col++)
-            {
-                if (buttons[row][col].getText().equals(""))
-                {
+
+    private boolean isBoardFull() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (buttons[row][col].getText().equals("")) {
                     return false;
                 }
             }
         }
         return true;
     }
+
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(() -> 
+        {
+            TicTacToeGame game = new TicTacToeGame();
+            game.setVisible(true);
+        });
+    } 
+
 }
